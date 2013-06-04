6d2e6e3d4ef07b6f50a9ca6aa7b390b47f9c32fc
matmsh <matmsh@yahoo.com>
2013-06-04 18:42:11 +0100

    Use RowParser int DAO.

--- app/models/Motorcycle.scala
+++ app/models/Motorcycle.scala
@@ -3,9 +3,9 @@ package models
 import play.api.db.DB
 import anorm.Pk
 import anorm.NotAssigned
+import anorm.RowParser
 
-case class Motorcycle( id :Pk[Long]=NotAssigned, make: String, model: String, engineCapacity: Int)
-
+case class Motorcycle(id: Pk[Long] = NotAssigned, make: String, model: String, engineCapacity: Int)
 
 /**
  * A sort DAO for for the case class Motorcycle.
@@ -14,19 +14,15 @@ object Motorcycle {
 
   import anorm.SQL
   import anorm.SqlQuery
-
-  val sql: SqlQuery = SQL("select * from motorcycles order by make,model asc")
+  import anorm.ResultSetParser
 
   import play.api.Play.current
 
   def getAll: List[Motorcycle] = DB.withConnection {
-
     implicit connection =>
+      val sql: SqlQuery = SQL("select * from motorcycles order by make,model asc")
+      sql.as(motorcyclesParser)
 
-      sql().map(row =>
-
-        Motorcycle(row[Pk[Long]]("id"), row[String]("make"), row[String]("model"),
-          row[Int]("engineCapacity"))).toList
   }
 
   def insert(motorcycle: Motorcycle): Boolean = {
@@ -36,56 +32,63 @@ object Motorcycle {
             values (
             (select next value for motorcycles_seq), 
             {make}, {model}, {engineCapacity})""").on(
-            "make" -> motorcycle.make,
-            "model" -> motorcycle.model,
-            "engineCapacity" -> motorcycle.engineCapacity).executeUpdate()==1
+        "make" -> motorcycle.make,
+        "model" -> motorcycle.model,
+        "engineCapacity" -> motorcycle.engineCapacity).executeUpdate() == 1
     }
   }
 
-  def update(id:Long, motorcycle: Motorcycle):Boolean = {
+  def update(id: Long, motorcycle: Motorcycle): Boolean = {
     DB.withConnection { implicit connection =>
       SQL("""update motorcycles
             set make = {make}, model = {model}, engineCapacity = {engineCapacity}
             where id = {id}""").on(
-          "make"-> motorcycle.make,
-          "model" -> motorcycle.model,
-          "engineCapacity"-> motorcycle.engineCapacity,
-          "id" -> id)
-          .executeUpdate() ==1
+        "make" -> motorcycle.make,
+        "model" -> motorcycle.model,
+        "engineCapacity" -> motorcycle.engineCapacity,
+        "id" -> id)
+        .executeUpdate() == 1
     }
   }
-  
-  
-  def findByMakeAndModel(motorcycle:Motorcycle):Option[Motorcycle] = 
+
+  def findByMakeAndModel(motorcycle: Motorcycle): Option[Motorcycle] =
     DB.withConnection { implicit connection =>
       val sql = SQL("select * from motorcycles where make={make} and model={model}").
-      on("make" -> motorcycle.make,
-         "model" -> motorcycle.model
-      )
-      
-     sql().map(row =>
-        Motorcycle(row[Pk[Long]]("id"), row[String]("make"), row[String]("model"),
-          row[Int]("engineCapacity"))).headOption
-            
-  }
-  
- def findById(id: Long): Option[Motorcycle] = {
-    DB.withConnection { implicit connection =>
-      val sql =SQL("select * from motorcycles where id = {id}").on('id -> id);
-       sql().map(row =>
+        on("make" -> motorcycle.make,
+          "model" -> motorcycle.model)
 
-        Motorcycle(row[Pk[Long]]("id"), row[String]("make"), row[String]("model"),
-          row[Int]("engineCapacity"))).headOption
-      
+      sql.as(motorcyclesParser).headOption
+    }
+
+  def findById(id: Long): Option[Motorcycle] = {
+    DB.withConnection { implicit connection =>
+      val sql = SQL("select * from motorcycles where id = {id}").on('id -> id);
+      sql.as(motorcyclesParser).headOption
     }
   }
- 
- def delete(id: Long) = {
+
+  def delete(id: Long) = {
     DB.withConnection { implicit connection =>
       SQL("delete from motorcycles where id = {id}").on('id -> id).executeUpdate()
     }
   }
- 
+
+  val motorcycleParser: RowParser[Motorcycle] = {
+    import anorm.~
+    import anorm.SqlParser._
+    get[Pk[Long]]("id") ~
+      str("make") ~
+      str("model") ~
+      int("engineCapacity") map {
+        case id ~ make ~ model ~ engineCapacity =>
+          Motorcycle(id, make, model, engineCapacity)
+      }
+  }
+
+  val motorcyclesParser: ResultSetParser[List[Motorcycle]] = {
+    motorcycleParser *
+  }
+
 }
 
 
---
+++ app/models/MotorcycleOld.scala
@@ -0,0 +1,95 @@
+package models
+
+import play.api.db.DB
+import anorm.Pk
+import anorm.NotAssigned
+
+//case class Motorcycle( id :Pk[Long]=NotAssigned, make: String, model: String, engineCapacity: Int)
+
+
+/**
+ * A sort DAO for for the case class Motorcycle.
+ */
+object MotorcycleOld {
+
+  import anorm.SQL
+  import anorm.SqlQuery
+
+  val sql: SqlQuery = SQL("select * from motorcycles order by make,model asc")
+
+  import play.api.Play.current
+
+  def getAll: List[Motorcycle] = DB.withConnection {
+
+    implicit connection =>
+
+      sql().map(row =>
+
+        Motorcycle(row[Pk[Long]]("id"), row[String]("make"), row[String]("model"),
+          row[Int]("engineCapacity"))).toList
+  }
+
+  def insert(motorcycle: Motorcycle): Boolean = {
+    DB.withConnection { implicit connection =>
+      SQL("""insert
+            into motorcycles
+            values (
+            (select next value for motorcycles_seq), 
+            {make}, {model}, {engineCapacity})""").on(
+            "make" -> motorcycle.make,
+            "model" -> motorcycle.model,
+            "engineCapacity" -> motorcycle.engineCapacity).executeUpdate()==1
+    }
+  }
+
+  def update(id:Long, motorcycle: Motorcycle):Boolean = {
+    DB.withConnection { implicit connection =>
+      SQL("""update motorcycles
+            set make = {make}, model = {model}, engineCapacity = {engineCapacity}
+            where id = {id}""").on(
+          "make"-> motorcycle.make,
+          "model" -> motorcycle.model,
+          "engineCapacity"-> motorcycle.engineCapacity,
+          "id" -> id)
+          .executeUpdate() ==1
+    }
+  }
+  
+  
+  def findByMakeAndModel(motorcycle:Motorcycle):Option[Motorcycle] = 
+    DB.withConnection { implicit connection =>
+      val sql = SQL("select * from motorcycles where make={make} and model={model}").
+      on("make" -> motorcycle.make,
+         "model" -> motorcycle.model
+      )
+      
+     sql().map(row =>
+        Motorcycle(row[Pk[Long]]("id"), row[String]("make"), row[String]("model"),
+          row[Int]("engineCapacity"))).headOption
+            
+  }
+  
+ def findById(id: Long): Option[Motorcycle] = {
+    DB.withConnection { implicit connection =>
+      val sql =SQL("select * from motorcycles where id = {id}").on('id -> id);
+       sql().map(row =>
+
+        Motorcycle(row[Pk[Long]]("id"), row[String]("make"), row[String]("model"),
+          row[Int]("engineCapacity"))).headOption
+      
+    }
+  }
+ 
+ def delete(id: Long) = {
+    DB.withConnection { implicit connection =>
+      SQL("delete from motorcycles where id = {id}").on('id -> id).executeUpdate()
+    }
+  }
+ 
+}
+
+
+
+
+
+
